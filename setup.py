b84d779ca1ddd9c64daadd4a05613ef5b950b84a
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-06-24 14:58:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-python-app',
-    version='0.1.6',
+    version='0.1.8',
     packages=find_packages(),
     install_requires=[],  # Add dependencies here if needed
     entry_points={
