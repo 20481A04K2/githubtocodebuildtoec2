ebf8a3f9471cffc2369a70fa78887da20ed08ae1
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-06-24 15:18:37 +0530

    Update main.py

--- my_app/main.py
+++ my_app/main.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "🚀 Hello from CodeArtifact-based Python app in ec2!"
 
-if __name__ == "__main__":
+def main():
     app.run(host="0.0.0.0", port=8080)
+
+if __name__ == "__main__":
+    main()
